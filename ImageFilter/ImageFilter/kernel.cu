2e771639a23508798a7ed074c2d9db34b81e046a
Wendel <wendelblankjeske@gmail.com>
2019-05-29 03:40:58 -0300

    adicionado o filtro de convolução

--- ImageFilter/ImageFilter/kernel.cu
+++ ImageFilter/ImageFilter/kernel.cu
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include "CImg.h"
+#include <math.h>  
 
 #include <iostream>
 using namespace std;
@@ -12,12 +13,12 @@ using namespace cimg_library;
 
 void ProcessImage(unsigned int *color, unsigned int size);
 
-__host__ __device__ void PackageColor(unsigned int &color, int r, int g, int b)
+__host__ __device__ void PackColorBits(unsigned int &color, int r, int g, int b)
 {
 	color = ((r & 0x0ff) << 16) | ((g & 0x0ff) << 8) | (b & 0x0ff);
 }
 
-__host__ __device__ void DecomposeColor(int rgb, int &r, int &g, int &b)
+__host__ __device__ void UnpackColorBits(int rgb, int &r, int &g, int &b)
 {
 	r = (rgb >> 16) & 0x0ff;
 	g = (rgb >> 8) & 0x0ff;
@@ -32,15 +33,79 @@ __global__ void Filter(unsigned int *srcColor, unsigned int *dstColor)
 
 
 	int r, g, b;
-	DecomposeColor(srcColor[i], r, g, b);
+	UnpackColorBits(srcColor[i], r, g, b);
 
 	//r = 50;
 	//g = 50;
 	//b = 255;
 
 
-	PackageColor(dstColor[i], r, g, b);
+	PackColorBits(dstColor[i], r, g, b);
 }
+
+__global__ void Filter2(unsigned int *srcColor, unsigned int *dstColor)
+{
+	int i = blockIdx.x * blockDim.x + threadIdx.x;
+	//int j = blockIdx.y * blockDim.y + threadIdx.y;
+
+
+	int r, g, b;
+
+	const int filterWidth = 5;
+	const int filterHeight = 5;
+
+	float filter[filterWidth][filterHeight] =
+	{
+		0,  0, -1,  0,  0,
+		0,  0, -1,  0,  0,
+		0,  0,  2,  0,  0,
+		0,  0,  0,  0,  0,
+		0,  0,  0,  0,  0,
+	};
+
+	const int midX = floor(float(filterWidth));
+	const int midY = floor(float(filterHeight));
+
+	float factor = 1.0;
+	float bias = 0;
+
+	int w = 1360;
+	int h = 768;
+
+	//apply the filter
+
+	double red = 0.0, green = 0.0, blue = 0.0;
+
+	//multiply every value of the filter with corresponding image pixel
+	for (int filterY = 0; filterY < filterWidth; filterY++)
+	{
+		for (int filterX = 0; filterX < filterHeight; filterX++)
+		{
+
+
+			int index = i + filterX - midX + w * (filterY - midY);
+
+			UnpackColorBits(srcColor[index], r, g, b);
+			red += r * filter[filterY][filterX];
+			green += g * filter[filterY][filterX];
+			blue += b * filter[filterY][filterX];
+		}
+	}
+
+	//truncate values smaller than zero and larger than 255
+	r = min(max(int(factor * red + bias), 0), 255);
+	g = min(max(int(factor * green + bias), 0), 255);
+	b = min(max(int(factor * blue + bias), 0), 255);
+
+
+	PackColorBits(dstColor[i], r, g, b);
+}
+
+
+
+
+
+
 
 void SetImageColor(CImg<unsigned char> &image, unsigned int *color, int width, int height)
 {
@@ -55,7 +120,7 @@ void SetImageColor(CImg<unsigned char> &image, unsigned int *color, int width, i
 			//newImage[y * width + x] = 2000;
 
 			int r, g, b;
-			DecomposeColor(color[k], r, g, b);
+			UnpackColorBits(color[k], r, g, b);
 
 			image(x, y, 0) = r;
 			image(x, y, 1) = g;
@@ -72,32 +137,13 @@ void SetColorArray(unsigned int *color, CImg<unsigned char> image, int width, in
 	{
 		for (int y = 0; y < height; y++)
 		{
-			 PackageColor(color[y * width + x],	image(x, y, 0), image(x, y, 1), image(x, y, 2));
+			PackColorBits(color[y * width + x], image(x, y, 0), image(x, y, 1), image(x, y, 2));
 		}
 	}
 }
 
 int main()
 {
-	//const int arraySize = 5;
-	//const int a[arraySize] = { 1, 2, 3, 4, 5 };
-	//const int b[arraySize] = { 10, 20, 30, 40, 50 };
-	//int c[arraySize] = { 0 };
-
-	//// Add vectors in parallel.
-	//addWithCuda(c, a, b, arraySize);
-
-
-	//printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-	//	c[0], c[1], c[2], c[3], c[4]);
-
-	//// cudaDeviceReset must be called before exiting in order for profiling and
-	//// tracing tools such as Nsight and Visual Profiler to show complete traces.
-	//cudaDeviceReset();
-
-
-
-
 
 	int width = 1360;
 	int height = 768;
@@ -107,17 +153,24 @@ int main()
 	CImg<unsigned char> newImage(width, height, 1, 3, 255);// = image;
 
 
+	int filter[3][3] =
+	{
+	   0, 0, 0,
+	   0, 1, 0,
+	   0, 0, 0
+	};
+
 	unsigned int *color = new unsigned int[width * height];
 
+
+
+
 	SetColorArray(color, image1, width, height);
-
-
-	
-
 	ProcessImage(color, width * height);
 	SetImageColor(newImage, color, width, height);
 
 	CImgDisplay window(width, height);
+
 	while (!window.is_closed())
 	{
 		if (window.is_keyESC())
@@ -132,7 +185,7 @@ int main()
 		}
 
 
-		
+
 		window.display(newImage);
 
 		window.wait();
@@ -164,7 +217,7 @@ void ProcessImage(unsigned int *color, unsigned int size)
 
 
 	// Launch a kernel on the GPU with one thread for each element.
-	Filter << <1360, 768 >> > (dev_color, dev_new_color);
+	Filter2 << <size / 512, 512 >> > (dev_color, dev_new_color);
 
 
 	cudaDeviceSynchronize();
